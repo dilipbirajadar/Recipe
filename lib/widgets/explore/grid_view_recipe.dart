d3aaea59c6fb8edac7c87acd313c7628f2f4af47
Dilip Patil <dilip.birajadar@gmail.com>
2023-01-17 21:51:51 +0530

    added offline support.

--- lib/widgets/explore/grid_view_recipe.dart
+++ lib/widgets/explore/grid_view_recipe.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:recipe/screens/recipe_item_details_screen.dart';
 import 'package:recipe/services/api_helper.dart';
 import 'package:recipe/shared/color.dart';
@@ -15,6 +18,7 @@ class recipegridview extends StatefulWidget {
 
 class _recipegridviewState extends State<recipegridview> {
   late Future<List<RecipeData>> _future;
+  late List<RecipeData> _localData;
   final String urlExplore =
       "https://flutter-receipe-default-rtdb.firebaseio.com/recipes.json";
   final String urlFavorite =
@@ -31,6 +35,7 @@ class _recipegridviewState extends State<recipegridview> {
     //   url = urlExplore;
     // }
     _future = ApiHelper().getRecipe(widget.isFavScreen);
+    readJson();
   }
 
   @override
@@ -94,11 +99,70 @@ class _recipegridviewState extends State<recipegridview> {
               ),
             );
           } else if (snapshot.hasError) {
-            return Text(snapshot.error.toString());
+            //return Text(snapshot.error.toString());
+            return SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  GridView.builder(
+                    shrinkWrap: true,
+                    physics: BouncingScrollPhysics(),
+                    gridDelegate:
+                    const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 5,
+                    ),
+                    itemCount: _localData.length,
+                    itemBuilder: ((context, index) {
+                      return Container(
+                        margin: EdgeInsets.all(15),
+                        child: Card(
+                          semanticContainer: true,
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          elevation: 0.2,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0)),
+                          child: InkWell(
+                            onTap: () {
+                              print("card ${index} is tapped.");
+                              Navigator.pushNamed(
+                                context,
+                                '/recipe_details',
+                                arguments: {
+                                  'recipe_data': _localData[index],
+                                  'index': index,
+                                },
+                              );
+                              //RecipeDetailsScreen();
+                            },
+                            child: //Text('${snapshot.data?[index].name}'),
+                            Image.asset(
+                              'images/${_localData[index].imageName}.png',
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+                  ),
+                ],
+              ),
+            );
           }
           return const Text("Loading...");
         }),
       ),
     );
+  }
+
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString('assets/data.json');
+    print('json response : $response');
+    List<dynamic> body = jsonDecode(response);
+    List<RecipeData> clients = body.map((dynamic data) => RecipeData.fromJson(data)).toList();
+    setState(() {
+      _localData = clients ;
+    });
   }
 }
